93461cce11aecd4820a390380d591570a4df684e
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-05-12 00:59:50 +0200

    start js

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+      pwShowHide = document.querySelectorAll(".showHidePw"),
+      pwFields = document.querySelectorAll(".password");
+
+
+    //   js code to show/hide password and change icon
+    
