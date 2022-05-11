43c00a2665d2b34c865465131681338aabd4beab
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-05-12 01:20:49 +0200

    js if else condiditon

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ const container = document.querySelector(".container");
 
 
     //   js code to show/hide password and change icon
-    
+    pwShowHide.forEach(eyeIcon =>{
+        eyeIcon.addEventListener("click", ()=> {
+            pwFields.forEach(pwFields => {
+                if(pwFields.type === "password"){
+                    pwFields.type = "text";
+                } else{
+                    pwFields.type = "password";
+                }
+            })
+        } )
+    })
