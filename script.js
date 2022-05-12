3c273fb04e6f8f32a71c1248404df9c8c147b408
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-05-12 02:14:06 +0200

    sign up

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".container"),
       pwShowHide = document.querySelectorAll(".showHidePw"),
-      pwFields = document.querySelectorAll(".password");
+      pwFields = document.querySelectorAll(".password"),
+      signUp = document.querySelector(".signup-link "),
+      login = document.querySelector(".login-link");
 
 
     //   js code to show/hide password and change icon
@@ -9,9 +11,25 @@ const container = document.querySelector(".container");
             pwFields.forEach(pwFields => {
                 if(pwFields.type === "password"){
                     pwFields.type = "text";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye-slash","uil-eye");
+                    })
                 } else{
                     pwFields.type = "password";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye","uil-eye-slash");
+                    })
                 }
             })
         } )
     })
+
+    // js code to appear signup and login form
+    signUp.addEventListener("click", () => {
+        container.classList.add("active");
+    });
+    login.addEventListener("click", () => {
+        container.classList.remove("active");
+    });
